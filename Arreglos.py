1bc53e41ae5e7108ee60b31addfb752931a3e696
Azhafar <87744378+AzhafarZ@users.noreply.github.com>
2021-10-26 19:22:39 -0500

    Update Arreglos.py

--- Arreglos.py
+++ Arreglos.py
@@ -12,6 +12,8 @@ Ingresa 0 para salir del programa.
 
 °°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°°
 """
+valores = []
+valores_ordenados=[]
 print (interfaz)
 opcion = int(input())
 #Ciclo repetitivo del menú
@@ -21,27 +23,41 @@ while opcion !=0:
         #Defino la longitud del arreglo mediante la variable de valor entero TAMAÑO.
         print ("¿Cuántos valores desea ingresar?")
         tamaño = int(input())
-        longitud = [0 for i in range(tamaño)]
         #Ingreso de valores mediante un blucle for.
-        for i in range(len(longitud)):
+        for i in range(tamaño):
             print("Ingrese el valor número %d: " %(i+1), end="")
-            longitud[i] = input()
+            valores.append (input())
         print ("")
         print ("Tus valores han sido capturados")
         print ("")
     elif opcion == 2:
-        orden = sorted(longitud)
-        print ("")
-        print ("Los valores han sido ordenados")
-        print ("")
+        if valores:
+            valores_ordenados = sorted(valores)
+            print ("")
+            print ("Los valores han sido ordenados")
+            print ("")
+        else:
+            print ("")
+            print ("No hay valores que pueda ordenar")
+            print ("")
     elif opcion == 3:
-        print ("")
-        print ("Tus valores originales son los siguientes \n:", longitud)
-        print ("")
+        if valores:
+            print ("")
+            print ("Tus valores originales son los siguientes \n:", valores)
+            print ("")
+        else:
+            print ("")
+            print ("No hay valores que pueda mostrar")
+            print ("")
     elif opcion == 4:
-        print ("")
-        print ("Tus valores ordenados son los siguientes \n:", orden)
-        print ("")
+        if valores_ordenados:
+            print ("")
+            print ("Tus valores ordenados son los siguientes \n:", valores_ordenados)
+            print ("")
+        else:
+            print ("")
+            print ("Tus valores aun no han sido ordenados")
+            print ("")
     elif opcion == 5:
         print("¿Desea salir del programa?")
         print ("Sí: Presióne 1")
@@ -50,13 +66,11 @@ while opcion !=0:
         if salir <= 1:
             print("El programa está finalizandose")
         #Evita que el ciclo while sea infinito.
-        else:
-                print (interfaz)
-                opcion = int(input())
+            break
 #Evita que el usuario ingrese respuestas no apropiadas.
     else:
         print("Esa opción no es valida")
     print (interfaz)
     opcion = int(input())
-    
+
 print("Saliendo del programa.")
